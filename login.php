c929b1ee8740313a1741a5889c27693e45aa0989
cputrief <cutputriefrina@gmail.com>
2024-12-21 10:42:19 +0700

    proses login

--- login.php
+++ login.php
@@ -1,16 +1,19 @@
 <?php
+$pesan = "";
 if(isset($_POST['tombol'])){
   # 1. koneksi database
   include_once("koneksi.php");
   # 2. mengambil value dari input
   $email = $_POST['email'];
-  $pass = $_POST['pass'];
+  $psw = md5($_POST['psw']);
   
   # 3. tuliskan query pengecekan apakag data login ada di database?
-  $sql_cek = "SELECT * FROM users WHERE email='$email' AND password='$pass'";
+  $sql_cek = "SELECT * FROM users WHERE email='$email' AND password='$psw'";
 
   # 4. menjalankan query di atas
   $qry_cek = mysqli_query($koneksi,$sql_cek);
+
+
 
   # 5. pengecekan lanjutan
   $cek = mysqli_num_rows($qry_cek);
@@ -18,13 +21,33 @@ if(isset($_POST['tombol'])){
   # 6. buatkan if jika login berhasil atau gagal
   if($cek > 0){
     // login berhasil
+      # 4.1. mengambil data lainnya dari tabel users berdasarkan data login
+      $ambil = mysqli_fetch_array($qry_cek);
+      $nama_login = $ambil['nama'];
+      $id_login = $ambil['id'];
+    //pembuatan session
+    $_SESSION['sid'] = $id_login;
+    $_SESSION['semail'] = $email;
+    $_SESSION['snama'] = $nama_login;
+
+  // pembuatan cookie
+  if($_post['cek'] == "yes"){
+    setcookie("cid",$id_login,time()+(60*60*24*90),"/");
+    setcookie("cemail",$email,time()+(60*60*24*90),"/");
+    setcookie("cnama",$nama_login,time()+(60*60*24*90),"/");
+  }
+    header("location:index.php");
+
+
   }else{
     // login gagal
+    $pesan = '<div class="alert alert-danger" role="alert"><i class="fa-solid fa-triangle-exclamation"></i>
+    Login Gagal, coba lagi!!
+  </div>';
   }
 }
 
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -33,12 +56,14 @@ if(isset($_POST['tombol'])){
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Project IS62</title>
     <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/all.css">
 </head>
 <body>
 
 
 <div class="container">
     <div class="row mt-5">
+          <?=$pesan?>
         <div class="col-6 m-auto">
             <div class="card">
             <div class="card-header text-center">
@@ -68,6 +93,7 @@ if(isset($_POST['tombol'])){
 </div>
 
     <script src="js/bootstrap.js"></script>
+    <script src="js/all.js"></script>
     <script src="js/bootstrap.bundle.js"></script>
 </body>
 </html>
